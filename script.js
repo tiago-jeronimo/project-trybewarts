73a96fd6cc1b2ef2a5570d97b87904ff1662e111
Leandro Paiva <leandroapaiva@yahoo.com>
2022-01-15 19:20:16 -0300

    trabalhando no item 3

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 /* const { RuleTester } = require("eslint"); */
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const btn = document.querySelector('#send');
 
 const pushBtn = document.getElementById('submit-btn');
 const agreementState = document.getElementById('agreement');
@@ -12,3 +15,12 @@ function pushBtnState() {
   }
 }
 agreementState.addEventListener('click', pushBtnState);
+
+function loginCheck() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+btn.addEventListener('click', loginCheck);
